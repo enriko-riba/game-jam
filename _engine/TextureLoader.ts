f1a4d578f14dd223b5441d14b863342fb74618f6
eriba <enriko.riba@gmail.com>
2019-01-06 16:25:20 +0100

    rebuild

--- _engine/TextureLoader.ts
+++ _engine/TextureLoader.ts
@@ -1,38 +1,42 @@
 import { PIXI } from "..";
-export class TextureLoader{
 
+/**
+ * 
+ */
+export class TextureLoader {
+
+    /**
+     * simple cache used to apply mipmap and scaleMode only the first time an atlas is loaded.[]
+     */
     private static resourceCache = {};
 
     public static Get = (fullName: string, mipmap: boolean = false): PIXI.Texture | null => {
         var idx = fullName.indexOf('.json@');
-        var textureName = (idx > 0) ? fullName.substr(idx + 6) : fullName; 
-        var resourceName =    (idx > 0) ? fullName.substr(0, idx + 5) : fullName;   
-        if(!TextureLoader.resourceCache[resourceName]) {
+        var textureName = (idx > 0) ? fullName.substr(idx + 6) : fullName;
+        var resourceName = (idx > 0) ? fullName.substr(0, idx + 5) : fullName;
+        if (!TextureLoader.resourceCache[resourceName]) {
             TextureLoader.resourceCache[resourceName] = PIXI.loader.resources[resourceName];
             TextureLoader.resourceCache[resourceName].spritesheet.baseTexture.mipmap = mipmap;
+            TextureLoader.resourceCache[resourceName].spritesheet.baseTexture.scaleMode = PIXI.SCALE_MODES.LINEAR;
         }
         var res = TextureLoader.resourceCache[resourceName];
 
-        if(!res){
+        if (!res) {
             console.error(`Resource:'${fullName}' not found!`);
             return null;
         }
 
-        if(res.loadType == 2){
-            return res.texture;
-        } else if(res.loadType == 1){
-            
-            var t = res.textures[textureName];
-            t.baseTexture.scaleMode = PIXI.SCALE_MODES.LINEAR;
-            //t.baseTexture.mipmap = false;
-            return t;
-        }else{
-            console.error(`Resource:'${fullName}' unknown load type!`, res);
-        }  
-        return null;      
+        switch (res.loadType) {
+            case 1: //  atlas
+                return res.textures[textureName];
+            case 2: //  single texture
+                return res.texture;
+        }
+        console.error(`Resource:'${fullName}' unknown load type!`, res);
+        return null;
     }
 
-    public static IsAtlas(texture:PIXI.Texture) {
+    public static IsAtlas(texture: PIXI.Texture) {
         return texture.frame.width != texture.baseTexture.width || texture.frame.height != texture.baseTexture.height;
     }
 }
