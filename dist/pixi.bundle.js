8be287bf4c6ec721e54d48b20fd76fe4f828ff78
Enriko Riba <enriko.riba@gmail.com>
2020-02-09 18:15:58 +0100

    production rebuild

--- dist/pixi.bundle.js
+++ dist/pixi.bundle.js
@@ -1,16 +1,2 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["pixi"],[
-/* 0 */,
-/* 1 */
-/*!**********************************!*\
-  !*** multi pixi.js pixi-filters ***!
-  \**********************************/
-/*! no static exports found */
-/*! all exports used */
-/***/ (function(module, exports, __webpack_require__) {
-
-__webpack_require__(/*! pixi.js */"./node_modules/pixi.js/lib/pixi.es.js");
-module.exports = __webpack_require__(/*! pixi-filters */"./node_modules/pixi-filters/lib/pixi-filters.esm.js");
-
-
-/***/ })
-],[[1,"runtime","vendors~main~pixi","vendors~pixi"]]]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{134:function(n,o,p){p(7),n.exports=p(135)}},[[134,0,2,6]]]);
+//# sourceMappingURL=pixi.bundle.js.map
